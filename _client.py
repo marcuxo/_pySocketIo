1379b59bb8b4281dca858f62f0292ee684960b81
marcuxo <marcourrutiam@gmail.com>
2022-02-11 15:04:14 -0300

    🐍🐍🐍🐍🐍🐍🐍

---
+++ _client.py
@@ -0,0 +1,60 @@
+import socketio
+import os
+import subprocess
+import time
+
+
+sio = socketio.Client()
+sio.connect('http://localhost:8080')
+print('my sid is', sio.sid)
+
+@sio.event
+def connect():
+    sio.connect('http://localhost:8080')
+    print('my sid is', sio.sid)
+    print("I'm connected!")
+    #message()
+    
+#sio.emit('message','timelapse')
+
+@sio.event
+def on_message(data):
+    print('I received a message1!')
+#sio.emit('message', 'toserver')
+@sio.event
+def message(data):
+    #sio.emit('message','mtto.png', room='chat_users', skip_sid=sio.sid)
+    print(os.getcwd())
+    #os.system('cd C:/Users/murrutiam/Desktop/apk/scada/ && run.cmd')
+    #os.system('echo ifconfig')
+    #os.system('npm run dev')
+    #cmd = 'cd C:/Users/murrutiam/Desktop/apk/scada/ && npm run dev'
+    #os.popen('cd C:/Users/murrutiam/Desktop/apk/scada/ && npm run dev')
+    
+    #time.sleep(30)
+    print(data,'*-*')
+    return
+    
+    
+@sio.on('screenshot')
+def catch_all(data):
+    print(data,"**")
+    directorio = os.getcwd()
+    with open('../scada/src/public/scada1.png', 'rb') as f:
+        image_data = f.read()
+    sio.emit('remote', {'image_data': image_data})
+    pass
+
+
+
+@sio.event
+def connect_error(data):
+    print("The connection failed!3")
+
+@sio.event
+def disconnect():
+    print("I'm disconnected4!")
+
+#connect()
+#message('hola mundo')
+#sio.disconnect()
---
+++ _socketio36.py
@@ -0,0 +1,43 @@
+from aiohttp import web
+import socketio
+
+## creates a new Async Socket IO Server
+sio = socketio.AsyncServer()
+## Creates a new Aiohttp Web Application
+app = web.Application()
+# Binds our Socket.IO server to our Web App
+## instance
+sio.attach(app)
+
+## we can define aiohttp endpoints just as we normally
+## would with no change
+async def index(request):
+    with open('index.html') as f:
+        return web.Response(text=f.read(), content_type='text/html')
+
+## If we wanted to create a new websocket endpoint,
+## use this decorator, passing in the name of the
+## event we wish to listen out for
+@sio.on('message')
+async def print_message(sid, message):
+    print("Socket ID: " , sid)
+    print(message)
+    ## await a successful emit of our reversed message
+    ## back to the client
+    await sio.emit('screenshot', message)
+
+@sio.on('remote')
+async def print_message(sid, message):
+    print("Socket ID: " , sid)
+    print(message)
+    ## await a successful emit of our reversed message
+    ## back to the client
+    await sio.emit('webpage', message)
+
+## We bind our aiohttp endpoint to our app
+## router
+app.router.add_get('/', index)
+
+## We kick off our server
+if __name__ == '__main__':
+    web.run_app(app)
